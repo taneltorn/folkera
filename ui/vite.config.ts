6863876e4c38182d17a43ac8ab2bab96414b77fe
Tanel Torn <tanel.torn@gmail.com>
2025-03-09 16:17:05 +0200

    wip

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,16 +1,16 @@
 /** @type {import('vite').UserConfig} */
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 const base = new URL(process.env.VITE_PUBLIC_URL || '/', 'http://localhost').pathname;
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    host: true,
-    port: 5173
-  },
-  plugins: [react()],
-  envDir: '../',
-  base: base || '/',
+    server: {
+        host: true,
+        port: 5173
+    },
+    plugins: [react()],
+    envDir: '../',
+    base: base || '/',
 })
