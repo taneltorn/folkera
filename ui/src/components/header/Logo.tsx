3c3d5710b6521b806ef32be888f48719ce116e46
Tanel Torn <tanel.torn@gmail.com>
2025-07-17 15:44:58 +0300

    wip

--- ui/src/components/header/Logo.tsx
+++ ui/src/components/header/Logo.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import {Button, Text} from "@mantine/core";
+import {Badge, Button, Text} from "@mantine/core";
 import {BsDiscFill} from "react-icons/bs";
 import {Size} from "../../utils/constants.ts";
+import packageJson from '../../../package.json';
 
 const Logo: React.FC = () => {
 
@@ -19,6 +20,9 @@ const Logo: React.FC = () => {
             <Text fz={size} fw={"bold"} ff={"Roboto"}>
                 ERA
             </Text>
+            <Badge size={"xs"} variant={"transparent"} pos={"absolute"} top={22} left={34}>
+                {packageJson.version}
+            </Badge>
         </Button>);
 }
 
