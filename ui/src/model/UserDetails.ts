ea8f935456ea32596b37bf901ac1b85c1f1f669c
Tanel Torn <tanel.torn@gmail.com>
2026-09-17 17:57:38 +0300

    wip

--- ui/src/model/UserDetails.ts
+++ ui/src/model/UserDetails.ts
@@ -6,6 +6,7 @@ export interface UserDetails {
     email: string;
     name: string;
     role: UserRole;
+    accessRefs: string[];
     createdAt: Date;
     isUser: boolean;
     isResearcher: boolean;
